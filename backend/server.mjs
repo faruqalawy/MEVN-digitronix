98df2182559e02803d2224014b803b964952c3df
faruqalawy <faruqhudaya@gmail.com>
2024-09-18 09:43:27 +0700

    configure docker

--- backend/server.mjs
+++ backend/server.mjs
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import flash from "connect-flash"
+import flash from "connect-flash";
 
 import passport from "passport";
 import LocalStrategy from "passport-local";
@@ -45,7 +45,7 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({
       mongoUrl: process.env.MONGODB_URL,
-      collectionName: 'sessions'
+      collectionName: "sessions",
     }),
     rolling: true, // Updates the session expiration time each time a request is made
     cookie: {
@@ -71,7 +71,7 @@ passport.deserializeUser(User.deserializeUser());
 //   next();
 // });
 
-app.use(flash())
+app.use(flash());
 
 // routes
 app.use("/", AuthRoute);
