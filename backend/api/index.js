084b6886dc2393decfc43f8f60bee255420b1905
faruqalawy <faruqhudaya@gmail.com>
2024-10-05 20:52:26 +0700

    change few things

--- backend/api/index.js
+++ backend/api/index.js
@@ -55,7 +55,6 @@ app.use(
       secure: "auto",
       httpOnly: true,
       maxAge: 30 * 24 * 60 * 60 * 1000,
-      sameSite: 'none'
     }, // 30 days in milliseconds (30 * 24 * 60 * 60)
   })
 );
